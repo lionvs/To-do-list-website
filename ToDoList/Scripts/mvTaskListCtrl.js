b09ed65926a36e2334b3f3664c00d2a938deb810
lionvs <vsyn@eliftech.com>
2015-06-05 18:07:21 +0300

    Undo task undo all subtaskes, clear textbox

--- ToDoList/Scripts/mvTaskListCtrl.js
+++ ToDoList/Scripts/mvTaskListCtrl.js
@@ -22,49 +22,54 @@
             $scope.tasksFinished.push(task);
             $http.put("/api/tasks/" + task.Id, task);
         }
-    }
-
+    };
     $scope.subtaskChanged = function(subtask, task) {
         subtask.IsFinished = !subtask.IsFinished;
         $http.put("/api/subtask/" + subtask.Id, subtask);
 
         //if all subtasks are finished move task to finished
         checkTaskFinish(task);
-    }
+    };
     $scope.removeSubtaskChanged = function (subtask, task) {
         task.subTasks.splice(task.subTasks.indexOf(subtask), 1);
         $http.delete("/api/subtask/" + subtask.Id);
         checkTaskFinish(task);
-    }
+    };
     $scope.removeAllFinishedTasks = function() {
         $scope.tasksFinished.forEach(function(task) {
             $http.delete("/api/tasks/" + task.Id);
         });
         $scope.tasksFinished = [];
-    }
+    };
     $scope.addnewSubtask = function(task, subTaskName) {
-        var subtask = {TaskId : task.Id, SubName:subTaskName, IsFinished:false};
+        var subtask = { TaskId: task.Id, SubName: subTaskName, IsFinished: false };
         task.subTasks.push(subtask);
         $http.post("/api/subtask/", subtask);
-    }
-
+    };
     $scope.addnewTask = function (taskName, newTaskCategory) {
         var task = { TaskName: taskName, subTasks: [], CategoryId: newTaskCategory.Id };
         $scope.tasksUnfinished.push(task);
         $http.post("/api/tasks/", task);
-    }
-
+    };
     $scope.taskChanged = function (task) {
         if ($scope.tasksFinished.indexOf(task) != -1) {
+
             $scope.tasksFinished.splice($scope.tasksFinished.indexOf(task), 1);
+            task.subTasks.forEach(function (entry) {
+                entry.IsFinished = false;
+                $http.put("/api/subtask/" + entry.Id, entry);
+            });
             $scope.tasksUnfinished.push(task);
+
         } else {
-            $scope.tasksUnfinished.splice($scope.tasksUnfinished.indexOf(task),1);
+            $scope.tasksUnfinished.splice($scope.tasksUnfinished.indexOf(task), 1);
             $scope.tasksFinished.push(task);
+          
+
+            
         }
         task.IsFinished = !task.IsFinished;
 
         $http.put("/api/tasks/" + task.Id, task);
-    }
-
+    };
 });
